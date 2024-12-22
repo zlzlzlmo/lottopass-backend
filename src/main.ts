a242c2b90c568689eb0f7d5524b77a68cfb04b37
zlzlzlmo <zlzlzlmo@daum.net>
2024-12-23 03:07:37 +0900

    CORS 개발 도메인 허용

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+    // CORS 설정
+    app.enableCors({
+      origin: 'http://localhost:5173', // 요청을 허용할 도메인
+      methods: 'GET,POST,PUT,DELETE', // 허용할 HTTP 메서드
+      credentials: true, // 쿠키 인증 정보 전송 허용
+    });
+  
   await app.listen(3000);
 }
 bootstrap();
